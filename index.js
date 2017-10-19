1b088e4a1df2a88007b833c7d7814ca3bf23c27f
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-19 15:58:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const logDriverNames = function (drivers){
 
 const logDriversByHometown = function (drivers, location) {
   drivers.forEach(function (driver) {
-    if (driver.hometown === location) {
+    if driver.hometown === location {
     console.log(driver.name)
   }
   })
