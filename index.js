bbff8c5bcade98d99f0784ca7988e968706d344c
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-19 15:54:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file!
 const logDriverNames = function (driver){
-  console.log(driver.name)
+  drivers.forEach(function (driver) {
+    console.log(driver.name)
+  })
 }
